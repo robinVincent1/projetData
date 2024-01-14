f1ffdb2323186a6453b02f2cda02fa01ac80c28e
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2024-01-14 20:08:15 +0100

    Setup

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
 import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+from pathlib import Path
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_excel(Path(__file__).parent / 'Extraction-finale_enquete-2023DS.xlsx')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(
+        id='genre-dropdown',
+        options=[{'label': i, 'value': i} for i in df['Genre'].dropna().unique()],
+        value=df['Genre'].dropna().unique()[0]
+    ),
     dcc.Graph(id='graph-content')
 ])
 
-@callback(
+@app.callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('genre-dropdown', 'value')
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(selected_genre):
+    dff = df[df['Genre'] == selected_genre]
+    fig = px.box(dff, x='Genre', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
+    return fig
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)

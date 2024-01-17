fbf7eecf2873f8e241681fee98c92d935678c663
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2024-01-17 21:09:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,57 +29,192 @@ colors = {
     'text': '#7FDBFF'
 }
 
+def create_genre_graph():
+    filtered_df = df[df['Genre'].isin(['Femme', 'Homme'])]
+    fig = px.box(filtered_df, x='Genre', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
+    return fig
+
+genre_graph = create_genre_graph()
+
+def create_employee_count_graph():
+    choices = ["De 50 à 249 salarié·e·s", "5 000 salarié·e·s ou plus", "De 20 à 49 salarié·e·s", "De 10 à 19 salarié·e·s ", "250 à  4 999 salarié·e·s ", " Moins de 10 salarié·e·s"]
+    filtered_df = df[df['Nombre de salarié·e·s de votre employeur / entreprise'].isin(choices)]
+    fig = px.box(filtered_df, x='Nombre de salarié·e·s de votre employeur / entreprise', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
+    return fig
+
+employee_count_graph = create_employee_count_graph()
+
+def create_executive_status_boxplot():
+    # Filtrer le DataFrame
+    filtered_df = df[df['Avez-vous un statut de cadre ou assimilé ?'].isin(['Oui', 'Non'])]
+
+    # Créer le boxplot
+    fig = px.box(filtered_df, x='Avez-vous un statut de cadre ou assimilé ?', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
+
+    return fig
+
+boxplot_cadre = create_executive_status_boxplot()
+
+def create_employer_status_boxplot():
+    # Filtrer le DataFrame
+    filtered_df = df[df['Quel est le statut de votre employeur / entreprise ?'].isin(['Secteur privé', 'Secteur public'])]
+
+    # Créer le boxplot
+    fig = px.box(filtered_df, x='Quel est le statut de votre employeur / entreprise ?', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
+
+    return fig
+
+boxplot_employer = create_employer_status_boxplot()
+
+
+
+def create_seaborn_plot():
+    # Sélectionner les données pour le graphique
+    salary_data = df['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].dropna()
+
+    # Créer le graphique
+    plt.figure(figsize=(10, 6))
+    sns.distplot(salary_data, hist=True, kde=True, 
+                 bins=int(180/5), color = 'darkblue', 
+                 hist_kws={'edgecolor':'black'},
+                 kde_kws={'linewidth': 4})
+
+    plt.title('Distribution des salaires brut annuel avec primes')
+    plt.xlabel('Salaire brut annuel avec primes'); plt.ylabel('Densité');
+
+    # Convert the matplotlib figure to a Plotly figure and return it
+    plotly_fig = tls.mpl_to_plotly(plt.gcf())
+    return plotly_fig
+
+loi_normale = create_seaborn_plot()
+# Callback to update the genre graph
+
+
+def create_education_graph():
+    dff = df.groupby('Formation')['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].mean().reset_index()
+    fig = px.bar(dff, x='Formation', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?',
+    labels={'Quel est votre salaire brut ANNUEL AVEC PRIMES ?': 'Average Annual Salary with Bonuses'})
+    return fig
+
+salaire_formation = create_education_graph()
+
+def create_pca_graph():
+    fig = px.scatter(pca_result_df, x='PC1', y='PC2', title='PCA Result')
+    return fig
+
+pca = create_pca_graph()
+
+def create_telework_days_bar_chart():
+    fig = px.bar(telework_salary_df, x='Combien de jours par semaine êtes-vous en télétravail ? ', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?',
+                 labels={'Combien de jours par semaine êtes-vous en télétravail ? ': 'Telework Days per Week',
+                         'Quel est votre salaire brut ANNUEL AVEC PRIMES ?': 'Average Annual Salary with Bonuses'})
+    return fig
+
+telework_days_bar_chart = create_telework_days_bar_chart()
+# Callback pour calculer et afficher le résultat de l'ANOVA
+
+
+def perform_telework_anova():
+    # Préparer les données pour l'ANOVA
+    anova_groups = [group['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].dropna() for name, group in df.groupby('Combien de jours par semaine êtes-vous en télétravail ? ')]
+
+    # Réaliser l'ANOVA
+    anova_result = stats.f_oneway(*anova_groups)
+
+    # Renvoyer le résultat de l'ANOVA
+    return html.Div([
+        html.P(f'ANOVA F-statistic: {anova_result.statistic:.2f}'),
+        html.P(f'ANOVA p-value: {anova_result.pvalue:.4f}')
+    ])
+
+telework_anova_result = perform_telework_anova()
+
+def create_correlation_heatmap():
+    # Sélectionner les colonnes pertinentes pour la corrélation
+    cols_for_correlation = ['Quel est votre salaire brut ANNUEL AVEC PRIMES ?', 'Combien de jours par semaine êtes-vous en télétravail ? ', 'Depuis combien de mois occupez-vous cet emploi ?']  # Ajoutez d'autres colonnes si nécessaire
+    correlation_df = df[cols_for_correlation].dropna()
+
+    # Calculer la matrice de corrélation
+    corr_matrix = correlation_df.corr()
+
+    # Créer la heatmap de corrélation
+    fig = px.imshow(corr_matrix, text_auto=True, aspect="auto", 
+                    labels=dict(x="Variable", y="Variable", color="Correlation"),
+                    x=corr_matrix.columns, y=corr_matrix.columns)
+
+    return fig
+
+correlation_heatmap = create_correlation_heatmap()
+
+def perform_clustering_analysis():
+    # Sélectionner et préparer les données pour le clustering
+    clustering_columns = ['Quel est votre salaire brut ANNUEL AVEC PRIMES ?','Combien de jours par semaine êtes-vous en télétravail ? ']
+    clustering_df = df[clustering_columns].dropna()
+    clustering_df = StandardScaler().fit_transform(clustering_df)
+
+    # Effectuer le clustering
+    kmeans = KMeans(n_clusters=4)  # Choisissez le nombre de clusters approprié
+    clusters = kmeans.fit_predict(clustering_df)
+
+    # Créer une visualisation pour les clusters
+    fig = px.scatter(x=clustering_df[:, 0], y=clustering_df[:, 1], color=clusters, 
+                     labels={'x': clustering_columns[0], 'y': clustering_columns[1]},
+                     title='Cluster Analysis of Graduates')
+
+    return fig
+
+clustering_result = perform_clustering_analysis()
 # App layout
 app.layout = html.Div([
     html.H1(children='Dashboard des analyses des salaires', style={'textAlign': 'center', "font-size": "300%"}),
-    dcc.Graph(id='seaborn-graph'),
+    dcc.Graph(id='seaborn-graph'
+              ,figure=loi_normale),
     html.Div([
         html.Div([
-    dcc.Graph(id='executive-status-boxplot'),
+    dcc.Graph(id='executive-status-boxplot',
+              figure=boxplot_cadre),
 ], className='six columns'),  # Utilisez les classes de grille CSS pour définir la largeur
     html.Div([
-     dcc.Graph(id='employer-status-boxplot'),
+     dcc.Graph(id='employer-status-boxplot'
+               ,figure=boxplot_employer),
 ], className='six columns'),  # Utilisez les classes de grille CSS pour définir la largeur
 ], className='row'),  # Utilisez la classe de grille CSS pour aligner les éléments horizontalement
     html.Div([
         html.Div([
             html.H2(children='Distribution des salaires par sexe', style={'textAlign': 'center'}),
-            dcc.Dropdown(
-                id='genre-dropdown',
-                options=[{'label': i, 'value': i} for i in df['Genre'].dropna().unique()],
-                value=df['Genre'].dropna().unique()[0]
-            ),
-            dcc.Graph(id='genre-graph'),
+            dcc.Graph(id='genre-graph',
+                      figure=genre_graph),
         ], className='six columns'),  # Utilisez les classes de grille CSS pour définir la largeur
 
         html.Div([
             html.H2(children="Distribution des salaires par taille d'entreprise", style={'textAlign': 'center'}),
-            dcc.Dropdown(
-                id='company-size-dropdown',
-                options=[{'label': i, 'value': i} for i in df['Nombre de salarié·e·s de votre employeur / entreprise'].dropna().unique()],
-                value=df['Nombre de salarié·e·s de votre employeur / entreprise'].dropna().unique()[0]
-            ),
-            dcc.Graph(id='company-size-graph'),
+            dcc.Graph(id='company-size-graph',
+                      figure=employee_count_graph),
         ], className='six columns'),  # Utilisez les classes de grille CSS pour définir la largeur
     ], className='row'),  # Utilisez la classe de grille CSS pour aligner les éléments horizontalement
 
     html.Div(id='anova-result'),
     html.H2(children="Distribution des salaires en fonction de la filiaire d'origine", style={'textAlign': 'center'}),
-    dcc.Graph(id='education-graph'),
+    dcc.Graph(id='education-graph',
+              figure=salaire_formation),
 
     html.H2(children='Principal Component Analysis (PCA)', style={'textAlign': 'center'}),
-    dcc.Graph(id='pca-graph'),
+    dcc.Graph(id='pca-graph',
+              figure=pca),
 
     html.H2(children='Salaire moyen en fonction du nombre de jours en télétravail', style={'textAlign': 'center'}),
-    dcc.Graph(id='telework-days-bar-chart'),
+    dcc.Graph(id='telework-days-bar-chart',
+              figure=telework_days_bar_chart),
 
     html.Div(id='telework-anova-result'),
 
     html.H2(children='Matrice des corrélations', style={'textAlign': 'center'}),
-    dcc.Graph(id='correlation-heatmap'),
+    dcc.Graph(id='correlation-heatmap',
+              figure=correlation_heatmap),
 
     html.H2(children='Analyse de clusters', style={'textAlign': 'center'}),
-    dcc.Graph(id='clustering-result'),
+    dcc.Graph(id='clustering-result',
+              figure=clustering_result),
 html.H2(children="L'influence de la situation géographique", style={'textAlign': 'center',"font-size": "500%"}),
 html.Div([
     html.Div([
@@ -104,180 +239,6 @@ html.Div([
 ], className='row'),
 ])
 
-@app.callback(
-    Output('executive-status-boxplot', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Utiliser un dropdown existant comme déclencheur
-     Input('company-size-dropdown', 'value')]
-)
-def update_executive_status_boxplot(_, __):
-    # Filtrer le DataFrame
-    filtered_df = df[df['Avez-vous un statut de cadre ou assimilé ?'].isin(['Oui', 'Non'])]
 
-    # Créer le boxplot
-    fig = px.box(filtered_df, x='Avez-vous un statut de cadre ou assimilé ?', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
-
-    return fig
-
-@app.callback(
-    Output('employer-status-boxplot', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Utiliser un dropdown existant comme déclencheur
-     Input('company-size-dropdown', 'value')]
-)
-def update_employer_status_boxplot(_, __):
-    # Filtrer le DataFrame
-    filtered_df = df[df['Quel est le statut de votre employeur / entreprise ?'].isin(['Secteur privé', 'Secteur public'])]
-
-    # Créer le boxplot
-    fig = px.box(filtered_df, x='Quel est le statut de votre employeur / entreprise ?', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?')
-
-    return fig
-
-@app.callback(
-    Output('seaborn-graph', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Use an existing dropdown as a trigger
-     Input('company-size-dropdown', 'value')]
-)
-def update_seaborn_graph(_, __):
-    return create_seaborn_plot()
-
-def create_seaborn_plot():
-    # Sélectionner les données pour le graphique
-    salary_data = df['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].dropna()
-
-    # Créer le graphique
-    plt.figure(figsize=(10, 6))
-    sns.distplot(salary_data, hist=True, kde=True, 
-                 bins=int(180/5), color = 'darkblue', 
-                 hist_kws={'edgecolor':'black'},
-                 kde_kws={'linewidth': 4})
-
-    plt.title('Distribution des salaires brut annuel avec primes')
-    plt.xlabel('Salaire brut annuel avec primes'); plt.ylabel('Densité');
-
-    # Convert the matplotlib figure to a Plotly figure and return it
-    plotly_fig = tls.mpl_to_plotly(plt.gcf())
-    return plotly_fig
-
-# Callback to update the genre graph
-@app.callback(
-    Output('genre-graph', 'figure'),
-    Input('genre-dropdown', 'value')
-)
-def update_genre_graph(selected_genre):
-    dff = df[df['Genre'] == selected_genre]
-    fig = px.box(dff, x='Genre', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?',
-                 labels={'Quel est votre salaire brut ANNUEL AVEC PRIMES ?': 'Annual Salary with Bonuses'})
-    return fig
-
-# Callback to update the company size graph and perform ANOVA
-@app.callback(
-    Output('company-size-graph', 'figure'),
-    Output('anova-result', 'children'),
-    Input('company-size-dropdown', 'value')
-)
-def update_company_size_graph(selected_size):
-    dff = df[df['Nombre de salarié·e·s de votre employeur / entreprise'] == selected_size]
-    fig = px.box(dff, x='Nombre de salarié·e·s de votre employeur / entreprise', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?',
-                 labels={'Quel est votre salaire brut ANNUEL AVEC PRIMES ?': 'Annual Salary with Bonuses'})
-
-    # Perform ANOVA for company size
-    anova_data = [df[df['Nombre de salarié·e·s de votre employeur / entreprise'] == size]['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].dropna() 
-                  for size in df['Nombre de salarié·e·s de votre employeur / entreprise'].dropna().unique()]
-    anova_result = stats.f_oneway(*anova_data)
-    anova_text = f'ANOVA Result for Company Size: F-statistic = {anova_result.statistic:.2f}, p-value = {anova_result.pvalue:.2e}'
-
-    return fig, anova_text
-
-# Callback to update the education graph
-@app.callback(
-    Output('education-graph', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Maintaining the dependency to refresh the graph.
-     Input('company-size-dropdown', 'value')]
-)
-def update_education_graph(_, __):
-    dff = df.groupby('Formation')['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].mean().reset_index()
-    fig = px.bar(dff, x='Formation', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?',
-    labels={'Quel est votre salaire brut ANNUEL AVEC PRIMES ?': 'Average Annual Salary with Bonuses'})
-    return fig
-
-@app.callback(
-    Output('pca-graph', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Maintaining the dependency to refresh the graph.
-     Input('company-size-dropdown', 'value')]
-)
-def update_pca_graph(_, __):
-    fig = px.scatter(pca_result_df, x='PC1', y='PC2', title='PCA Result')
-    return fig
-
-# Callback to update the telework days bar chart
-@app.callback(
-    Output('telework-days-bar-chart', 'figure'),
-    Input('genre-dropdown', 'value')  # Using an existing dropdown as a trigger
-)
-def update_telework_days_bar_chart(_):
-    fig = px.bar(telework_salary_df, x='Combien de jours par semaine êtes-vous en télétravail ? ', y='Quel est votre salaire brut ANNUEL AVEC PRIMES ?',
-                 labels={'Combien de jours par semaine êtes-vous en télétravail ? ': 'Telework Days per Week',
-                         'Quel est votre salaire brut ANNUEL AVEC PRIMES ?': 'Average Annual Salary with Bonuses'})
-    return fig
-
-# Callback pour calculer et afficher le résultat de l'ANOVA
-@app.callback(
-    Output('telework-anova-result', 'children'),
-    Input('genre-dropdown', 'value')  # Utiliser n'importe quel Input existant pour déclencher le callback
-)
-def perform_telework_anova(_):
-    # Préparer les données pour l'ANOVA
-    anova_groups = [group['Quel est votre salaire brut ANNUEL AVEC PRIMES ?'].dropna() for name, group in df.groupby('Combien de jours par semaine êtes-vous en télétravail ? ')]
-
-    # Réaliser l'ANOVA
-    anova_result = stats.f_oneway(*anova_groups)
-
-    # Renvoyer le résultat de l'ANOVA
-    return html.Div([
-        html.P(f'ANOVA F-statistic: {anova_result.statistic:.2f}'),
-        html.P(f'ANOVA p-value: {anova_result.pvalue:.4f}')
-    ])
-
-@app.callback(
-    Output('correlation-heatmap', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Utiliser un Input existant pour déclencher le callback
-     Input('company-size-dropdown', 'value')]
-)
-def update_correlation_heatmap(_, __):
-    # Sélectionner les colonnes pertinentes pour la corrélation
-    cols_for_correlation = ['Quel est votre salaire brut ANNUEL AVEC PRIMES ?', 'Combien de jours par semaine êtes-vous en télétravail ? ', 'Depuis combien de mois occupez-vous cet emploi ?']  # Ajoutez d'autres colonnes si nécessaire
-    correlation_df = df[cols_for_correlation].dropna()
-
-    # Calculer la matrice de corrélation
-    corr_matrix = correlation_df.corr()
-
-    # Créer la heatmap de corrélation
-    fig = px.imshow(corr_matrix, text_auto=True, aspect="auto", 
-                    labels=dict(x="Variable", y="Variable", color="Correlation"),
-                    x=corr_matrix.columns, y=corr_matrix.columns)
-
-    return fig
-
-@app.callback(
-    Output('clustering-result', 'figure'),
-    [Input('genre-dropdown', 'value'),  # Peut utiliser n'importe quel Input existant
-     Input('company-size-dropdown', 'value')]
-)
-def perform_clustering_analysis(_, __):
-    # Sélectionner et préparer les données pour le clustering
-    clustering_columns = ['Quel est votre salaire brut ANNUEL AVEC PRIMES ?','Combien de jours par semaine êtes-vous en télétravail ? ']
-    clustering_df = df[clustering_columns].dropna()
-    clustering_df = StandardScaler().fit_transform(clustering_df)
-
-    # Effectuer le clustering
-    kmeans = KMeans(n_clusters=4)  # Choisissez le nombre de clusters approprié
-    clusters = kmeans.fit_predict(clustering_df)
-
-    # Créer une visualisation pour les clusters
-    fig = px.scatter(x=clustering_df[:, 0], y=clustering_df[:, 1], color=clusters, 
-                     labels={'x': clustering_columns[0], 'y': clustering_columns[1]},
-                     title='Cluster Analysis of Graduates')
-
-    return fig
 
 app.run_server(debug=True)
